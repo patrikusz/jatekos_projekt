b629396c590e51784b6ef04a67517acd1aa33a8d
Nagy Erik <erik-n@mailbox.unideb.hu>
2025-10-16 12:55:58 +0200

    playercontroller

--- CarGame/Assets/Scripts/PlayerController.cs
+++ CarGame/Assets/Scripts/PlayerController.cs
@@ -5,7 +5,36 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    
+    public float speed, rotationSpeed;
+    Rigidbody rb;
+
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.maxLinearVelocity = speed;
+    }
+
+    void FixedUpdate()
+    {
+        float verticalInput = Input.GetAxis("Vertical");
+        rb.AddForce(transform.forward * speed * Time.deltaTime * verticalInput, ForceMode.Impulse);
+
+        //float horizontalInput = Input.GetAxis("Horizontal");
+        //rb.AddTorque(transform.up * rotationSpeed * Time.deltaTime * horizontalInput, ForceMode.Impulse);
+    }
+
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.up, horizontalInput * rotationSpeed * Time.deltaTime);
+
+        //float verticalInput = Input.GetAxis("Vertical");
+        //transform.Translate(Vector3.forward * speed * Time.deltaTime * verticalInput);
+
+    }
+
+
 }
 
 

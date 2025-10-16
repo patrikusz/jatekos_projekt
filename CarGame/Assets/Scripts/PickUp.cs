6992ceae3ec2d5ba0726f3e49ddaf06d500b5a99
Nagy Erik <erik-n@mailbox.unideb.hu>
2025-10-16 12:57:19 +0200

    pickup

--- CarGame/Assets/Scripts/PickUp.cs
+++ CarGame/Assets/Scripts/PickUp.cs
@@ -5,4 +5,23 @@ using UnityEngine;
 public class PickUp : MonoBehaviour
 {
 
+    GameObject gameManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = GameObject.Find("GameManager");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(this.gameObject);
+        gameManager.GetComponent<GameManager>().CoinCollector();
+        gameManager.GetComponent<GameManager>().SpawnCoin();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }

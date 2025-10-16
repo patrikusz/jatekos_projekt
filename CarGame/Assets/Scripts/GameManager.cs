a44eaa3acb2783f7f58cd63bd2abdd9b70e9cb61
Nagy Erik <erik-n@mailbox.unideb.hu>
2025-10-16 12:51:57 +0200

    map update, scripts

---
+++ CarGame/Assets/Scripts/GameManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+ 
+}
---
+++ CarGame/README.md
@@ -0,0 +1,2 @@
+# Game_dev_project_for_class_cars
+Simple car game
